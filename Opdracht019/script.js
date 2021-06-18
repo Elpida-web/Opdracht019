367826dc3a228445f29a497f4cb268f7e5877ca6
Elpida-web <84407349+Elpida-web@users.noreply.github.com>
2021-06-18 13:31:00 +0200

    Add files via upload

---
+++ Opdracht019/script.js
@@ -0,0 +1,32 @@
+//const checkAge = function (age) {
+ //   if (age >= 18) {
+  //  return Math.abs(age);
+ //   } 
+//    return age;
+//};
+
+//const greeting = funtion (age) {
+//if (age >= 18) {
+   // console.log("Hello there");
+//} else (false) 
+ //   console.log("Hey kiddo");
+//
+
+//checkAge(18);
+
+const mainPrice = function (amount) { 
+    return amount;
+};
+
+const priceIncl = function (amount) {
+    return amount * 1,21; 
+}
+return amount;
+
+const totalPrice = function (amount) {
+    const makeMainprice = mainPrice(amount);
+    const makePriceIncl = priceIncl(amount);
+    return priceVAT;
+};
+
+console.log(totalPrice(1000));
